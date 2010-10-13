b04e50a5c3a4ad5ebae6bec3a945a2e94107aef3
talklittle <talklittle@b202fef8-03fb-f63c-b751-98654c24a0e8>
2010-10-13 04:07:00 +0000

    somewhat working player. some randomness (bad). TODO regions (Geometry)

--- trunk/f10_4444_mosquito_v1/src/mosquito/g6/Geometry.java
+++ trunk/f10_4444_mosquito_v1/src/mosquito/g6/Geometry.java
@@ -2,6 +2,9 @@ package mosquito.g6;
 
 import java.awt.geom.Line2D;
 import java.awt.geom.Point2D;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
 
 import org.apache.log4j.Logger;
 
@@ -32,5 +35,51 @@ public class Geometry {
 		logger.debug("point ("+x0+","+y0+") line (("+x1+","+y1+"),("+x2+","+y2+")) distance="+distance);
 		
 		return distance;
+	}
+	
+	public static HashSet<HashSet<Line2D> > getRegions(Set<Line2D> walls) {
+		Set<Line2D> extendedWalls = getExtendedWalls(walls);
+		
+		// Map of intersection points to set of lines intersecting at that point
+		Map<Point2D, Set<Line2D> > intersectionMap = getIntersectionMap(extendedWalls);
+		// Trim the extended walls to share intersection points
+		Set<Line2D> trimmedWalls = trimToIntersections(extendedWalls);
+		
+		HashSet<HashSet<Line2D> > regions = new HashSet<HashSet<Line2D> >();
+		
+		Set<Point2D> checkedIntersections = new HashSet<Point2D>();
+		for (Point2D intersectionPoint : intersectionMap.keySet()) {
+			// check points that have not been checked yet
+			if (!checkedIntersections.contains(intersectionPoint)) {
+				checkedIntersections.add(intersectionPoint);
+				
+				for (Line2D wall : intersectionMap.get(intersectionPoint)) {
+					// get the other endpoint of the line
+					Point2D otherPoint = intersectionPoint.equals(wall.getP1()) ? wall.getP2() : wall.getP1();
+					checkedIntersections.add(intersectionPoint);
+				}
+			}
+		}
+		return regions;
+	}
+	
+//	private void getRegionsCheckingHelper(Map<Point2D, Set<Line2D> > intersectionMap,
+//			???, Set<Point2D> checkedIntersections) {
+//		
+//	}
+	
+	public static Set<Line2D> getExtendedWalls(Set<Line2D> walls) {
+		// TODO
+		return null;
+	}
+	
+	public static Map<Point2D, Set<Line2D> > getIntersectionMap(Set<Line2D> walls) {
+		// TODO
+		return null;
+	}
+	
+	public static Set<Line2D> trimToIntersections(Set<Line2D> walls) {
+		// TODO
+		return null;
 	}
 }
