95003e73222f8031cb9494590f15de9c729c7ec1
talklittle <talklittle@b202fef8-03fb-f63c-b751-98654c24a0e8>
2010-10-17 17:52:50 +0000

    WIP trying to do the simulated runs. Broken returning null for lights

--- f10_4444_mosquito_v1/src/mosquito/g6/MosquitoBuster.java
+++ f10_4444_mosquito_v1/src/mosquito/g6/MosquitoBuster.java
@@ -2,13 +2,14 @@ package mosquito.g6;
 
 import java.awt.geom.Line2D;
 import java.awt.geom.Point2D;
-import java.util.HashSet;
-import java.util.Iterator;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.Random;
 import java.util.Set;
 
 import mosquito.sim.Board;
 import mosquito.sim.Collector;
+import mosquito.sim.GameListener;
 import mosquito.sim.Light;
 import mosquito.sim.Player;
 
@@ -17,57 +18,166 @@ import org.apache.log4j.Logger;
 public class MosquitoBuster extends Player {
 	private int numLights;
 	private Set<Line2D> walls;
-	private Set<Light> lights;
 	private Board board;
 
+	// static members that are used in simulations
+	private static Set<Light> lights;
+	private static Point2D initialLightLocation;
 	
+
 	private static final Random random = new Random();
-	
+
 	private static final Logger logger = Logger.getLogger(MosquitoBuster.class);
-	
+
 	@Override
 	public String getName() {
 		return "Mosquito Buster";
 	}
-	
+
 	@Override
 	public void startNewGame(Set<Line2D> walls, int NumLights) {
 		this.walls = walls;
 		this.numLights = NumLights;
+		this.lights = getLightPositions();
+	}
+
+	@Override
+	public void startSimulatedGame(Set<Line2D> walls, int NumLights) {
+		this.walls = walls;
+		this.numLights = NumLights;
+	}
+
+	public Set<Light> getLightPositions() {
+		double[] startSpot = getStartSpot();
+		Set<Light> lightPositions = testLightPositions(startSpot[0], startSpot[1]);
+		return lightPositions;
 	}
 
 	/* Brute force to go through each spot on the board and test from there */
 	public double[] getStartSpot()
 	{
-		double[][] spots = new double[101][101];
+		double[][] spots = new double[2*101][2*101];
 		double[] fastestCoordinate = {0,0};
-		
+
 		for (double i = 0; i < 101; i+=0.5) {
 			for (double j = 0; j < 101; j+=0.5) {
-					spots[i][j] = testAtSpot(i, j); // test at spot does simulation in that coordinate
-					if(j-0.5 >= 0)
-					{
-						if((spots[i][j] <= spots[i][j-0.5]) && (spots[i][j] != -1.0));
-						fastestCoordinate[0] = i;
-						fastestCoordinate[1] = j;
-					}
+				spots[(int) (2*i)][(int) (2*j)] = testAtSpot(i, j); // test at spot does simulation in that coordinate
+				if(j-0.5 >= 0)
+				{
+					if((spots[(int) (2*i)][(int) (2*j)] <= spots[(int) (2*i)][(int) (2*(j-0.5))])
+							&& (spots[(int) (2*i)][(int) (2*j)] != -1.0));
+					fastestCoordinate[0] = i;
+					fastestCoordinate[1] = j;
+				}
 			}
 		}
-		
+
 		return fastestCoordinate;
-		
+
 	}
-	
+
+	/**
+	 * Run the tests 30 times and get the average number of rounds
+	 * @param x
+	 * @param y
+	 * @return
+	 */
+	public int testAtSpot(double x, double y) {
+
+		final ArrayList<Integer> simRounds = new ArrayList<Integer>();
+
+		initialLightLocation = new Point2D.Double(x, y);
+
+		for (int i = 0; i < 30; i++) {
+			double radians = i * (2.0 * Math.PI / 30.0);
+			lights = PutLights.putLights(walls, initialLightLocation, numLights, radians);
+
+			// run the test
+			runSimulation(3000, new GameListener() {
+				@Override
+				public void gameUpdated(GameUpdateType type) {
+					if(type.equals(GameUpdateType.MOVEPROCESSED))
+					{
+						logger.trace("We had a move happen, " + getSimulationRounds() +", caught: " + getSimulationNumCaught());
+					}
+					else if(type.equals(GameUpdateType.GAMEOVER))
+					{
+						logger.debug("Game ended at ticks: " + getSimulationRounds());
+						simRounds.add(getSimulationRounds());
+					}
+				}
+			});
+		}
+
+		// Get the sum, avg
+		int sum = 0;
+		for (Integer rounds : simRounds) {
+			sum += rounds;
+		}
+		int average = sum / simRounds.size();
+
+		return average;
+	}
+
+	/**
+	 * Same as testAtSpot but returns light config.
+	 * This method requires more memory so try to call it less often.
+	 * @param x
+	 * @param y
+	 * @return
+	 */
+	public Set<Light> testLightPositions(double x, double y) {
+		// map of testId => numRounds
+		final HashMap<Integer, Integer> mapRounds = new HashMap<Integer, Integer>();
+		// map of testId => Set<Light>
+		final HashMap<Integer, Set<Light> > mapLights = new HashMap<Integer, Set<Light> >();
+
+		initialLightLocation = new Point2D.Double(x, y);
+
+		for (int i = 0; i < 30; i++) {
+			final double radians = i * (2.0 * Math.PI / 30.0);
+			final int testId = i;
+			lights = PutLights.putLights(walls, initialLightLocation, numLights, radians);
+			mapLights.put(testId, lights);
+
+			// run the test
+			runSimulation(3000, new GameListener() {
+				@Override
+				public void gameUpdated(GameUpdateType type) {
+					if(type.equals(GameUpdateType.MOVEPROCESSED))
+					{
+						logger.info("We had a move happen, " + getSimulationRounds() +", caught: " + getSimulationNumCaught());
+					}
+					else if(type.equals(GameUpdateType.GAMEOVER))
+					{
+						logger.debug("Game ended at ticks: " + getSimulationRounds());
+						mapRounds.put(testId, getSimulationRounds());
+					}
+				}
+			});
+		}
+
+		// get the Set<Light> associated with the min
+		int minKey = 0;
+		int minRounds = 3000;
+		for (Integer key : mapRounds.keySet()) {
+			if (mapRounds.get(key) < minRounds) {
+				minRounds = mapRounds.get(key);
+				minKey = key;
+			}
+		}
+		return mapLights.get(minKey);
+	}
+
 	@Override
 	public Set<Light> getLights() {
-		
-		HashSet<Light> ret = new HashSet<Light>();
-		
-		ret = getLightPositions(); // after start spot has been chosen, we decide light positions and this returns them
-		
-		return ret;
+		return lights;
 	}
-	
+
+
+
+
+
 	/**
 	 * 
 	 * @param point The point
@@ -82,7 +192,7 @@ public class MosquitoBuster extends Player {
 		}
 		return false;
 	}
-	
+
 	private boolean isIntersectingLight(Point2D point) {
 		for (Light light : lights) {
 			if (point.distance(light.getLocation()) < 0.5)
@@ -93,21 +203,21 @@ public class MosquitoBuster extends Player {
 
 	@Override
 	public Collector getCollector() {
-		Point2D collectorLocation = new Point2D.Double(firstLight.getX()+0.1, firstLight.getY()+0.1);
+		Point2D collectorLocation = new Point2D.Double(initialLightLocation.getX()+0.1, initialLightLocation.getY()+0.1);
 		// move collector slightly if it's on a wall
 		while (isNearWall(collectorLocation, 0.5) && collectorLocation.getX() < 99 && collectorLocation.getY() < 99)
 			collectorLocation = new Point2D.Double(collectorLocation.getX()+0.1, collectorLocation.getY()+0.1);
 		// XXX if all else fails, use random location
 		while (isNearWall(collectorLocation, 0.5))
 			collectorLocation = new Point2D.Double(random.nextDouble() * 99.0, random.nextDouble() * 99.0);
-		
+
 		// move collector if it's on a light
 		while (isIntersectingLight(collectorLocation) && collectorLocation.getX() < 99 && collectorLocation.getY() < 99)
 			collectorLocation = new Point2D.Double(collectorLocation.getX()+0.1, collectorLocation.getY()+0.1);
 		// XXX if all else fails, use random location
 		while (isIntersectingLight(collectorLocation))
 			collectorLocation = new Point2D.Double(random.nextDouble() * 99.0, random.nextDouble() * 99.0);
-		
+
 		// for 1 light, just place it near the light
 		if (numLights == 1) {
 			Random r = new Random();
