f304b482ffbcd54ee17a8c1f1c0f1dc9b721dcb4
talklittle <talklittle@b202fef8-03fb-f63c-b751-98654c24a0e8>
2010-10-18 15:46:52 +0000

    quit if could not place any during a phase

--- trunk/f10_4444_mosquito_v1/src/mosquito/g6/PutLights.java
+++ trunk/f10_4444_mosquito_v1/src/mosquito/g6/PutLights.java
@@ -32,6 +32,7 @@ public class PutLights {
 			int numLightPerPhase;
 			while(numPlaced < numLight){
 				if(pedalIndex==(phase*8)){
+					// If we couldn't place any during this phase, quit
 					if (numPlacedThisPhase == 0) {
 						// TODO a good way to place the remaining lights
 						for ( ; numPlaced < numLight; numPlaced++) {
@@ -39,7 +40,9 @@ public class PutLights {
 								next = new Point2D.Double(random.nextDouble() * 100.0, random.nextDouble() * 100.0);
 							} while (CollideWithWall.isCollideWithWall(next, walls));
 							l = new Light(next.getX(), next.getY(), 1, 0, 0);
+							result.add(l);
 						}
+						return result;
 					}
 					pedalIndex = 0;
 					numPlacedThisPhase = 0;
