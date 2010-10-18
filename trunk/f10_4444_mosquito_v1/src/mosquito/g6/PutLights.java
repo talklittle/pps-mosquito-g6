72629bea5c9a21a920fb6b1e0eb27ff08a72c8f1
talklittle <talklittle@b202fef8-03fb-f63c-b751-98654c24a0e8>
2010-10-18 13:15:30 +0000

    get it working! kind of worried about speed. may take nearly an hour per game

--- trunk/f10_4444_mosquito_v1/src/mosquito/g6/PutLights.java
+++ trunk/f10_4444_mosquito_v1/src/mosquito/g6/PutLights.java
@@ -3,40 +3,59 @@ package mosquito.g6;
 import java.awt.geom.Line2D;
 import java.awt.geom.Point2D;
 import java.util.HashSet;
+import java.util.Random;
 import java.util.Set;
+
 import mosquito.sim.Light;
 
 public class PutLights {
 	
+	private static Random random = new Random();
+	
 	public static Set<Light> putLights(Set<Line2D> walls, Point2D base, int numLight, double initialRadient){
-		if(CollideWithWall.isCollideWithWall(base, walls)){
+		if(CollideWithWall.isCollideWithWall(base, walls) || OutOfBounds.isOutOfBounds(base)){
 			return null;
 		}else{
+			int numPlaced = 0;
 			Light l = new Light(base.getX(),base.getY(), 1,1,1);
 			Set<Light> result = new HashSet<Light>();
 			result.add(l);
 			if(numLight==1){
 				return result;
 			}
+			numPlaced++;
+			
 			Point2D next;
 			int phase = 1;
 			int pedalIndex = 0;
+			int numPlacedThisPhase = 0;
 			int numLightPerPhase;
-			while(numLight>0){
+			while(numPlaced < numLight){
 				if(pedalIndex==(phase*8)){
+					if (numPlacedThisPhase == 0) {
+						// TODO a good way to place the remaining lights
+						for ( ; numPlaced < numLight; numPlaced++) {
+							do {
+								next = new Point2D.Double(random.nextDouble() * 100.0, random.nextDouble() * 100.0);
+							} while (CollideWithWall.isCollideWithWall(next, walls));
+							l = new Light(next.getX(), next.getY(), 1, 0, 0);
+						}
+					}
 					pedalIndex = 0;
+					numPlacedThisPhase = 0;
 					phase++;
 				}
 				numLightPerPhase = Math.min(phase*8, numLight);
 				next = new Point2D.Double(base.getX()+((19.9*phase)*Math.cos(initialRadient+(pedalIndex*2*Math.PI/numLightPerPhase))),
 						base.getY()+((19.9*phase)*Math.sin(initialRadient+(pedalIndex*2*Math.PI/numLightPerPhase))));
-				if(CollideWithWall.isCollideWithWall(base, next, walls)){
+				if(CollideWithWall.isCollideWithWall(base, next, walls) || OutOfBounds.isOutOfBounds(next)){
 					pedalIndex++;
 				}else{
 					l = new Light(next.getX(), next.getY(), 60, 24, 24*(2-(phase-1)%3));
 					result.add(l);
 					pedalIndex++;
-					numLight--;
+					numPlaced++;
+					numPlacedThisPhase++;
 				}
 			}
 			return result;
