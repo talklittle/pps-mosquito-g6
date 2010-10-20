a376a00cb0fe768c48a22daeba15310f183c099f
pare_ku@hotmail.com <pare_ku@hotmail.com@b202fef8-03fb-f63c-b751-98654c24a0e8>
2010-10-20 07:04:18 +0000

    WIP putlights stuff

--- f10_4444_mosquito_v1/src/mosquito/g6/CollideWithWall.java
+++ f10_4444_mosquito_v1/src/mosquito/g6/CollideWithWall.java
@@ -4,8 +4,8 @@ import java.util.Set;
 
 public class CollideWithWall {
 
-	public static boolean isCollideWithWall(Point2D first, Point2D next, Set<Line2D> walls){
-		Line2D line = new Line2D.Double(first, next);
+	public static boolean isCollideWithWall(HelperLight first, HelperLight next, Set<Line2D> walls){
+		Line2D line = new Line2D.Double(first.getX(), first.getY(), next.getX(), next.getY());
 		for (Line2D l : walls) {
 			if(l.intersectsLine(line)){
 				return true;
@@ -14,7 +14,7 @@ public class CollideWithWall {
 		return false;
 	}
 	//Jon's function in Board
-	public static boolean isCollideWithWall(Point2D point, Set<Line2D> walls){
+	public static boolean isCollideWithWall(HelperLight point, Set<Line2D> walls){
 		double MOSQUITO_EPSILON = 0.5;
 		Rectangle2D pointRectangle = new Rectangle2D.Double(
 				point.getX() - MOSQUITO_EPSILON, point.getY() - MOSQUITO_EPSILON,
@@ -25,6 +25,22 @@ public class CollideWithWall {
 			}
 		}
 		return false;
+	}
+	
+	public static boolean isCollideWithOtherLights(HelperLight current, Set<HelperLight> lights){
+		
+		for (HelperLight l : lights) {
+			if(!current.getBase().equals(l.getBase())){
+				double distance = Math.sqrt(Math.pow(current.getX()-l.getX(), 2)+(Math.pow(current.getY()-l.getY(), 2)));
+				if(distance<=40){
+					if(!current.getBase().equals(l)){
+						return true;
+					}
+				}
+			}
+		}
+		return false;
+		
 	}
 	
 }
---
+++ f10_4444_mosquito_v1/src/mosquito/g6/HelperLight.java
@@ -0,0 +1,80 @@
+package mosquito.g6;
+
+import java.awt.geom.Point2D;
+
+import mosquito.sim.Light;
+
+public class HelperLight {
+	private double x, y, d, t, s;
+	private int phase = -1;
+	private HelperLight base = null;
+	
+	public HelperLight(double x, double y, double d, double t, double s) {
+		this.x = x;
+		this.y = y;
+		this.d = d;
+		this.t = t;
+		this.s = s;
+	}
+
+	public double getX() {
+		return x;
+	}
+
+	public void setX(double x) {
+		this.x = x;
+	}
+
+	public double getY() {
+		return y;
+	}
+
+	public void setY(double y) {
+		this.y = y;
+	}
+
+	public double getD() {
+		return d;
+	}
+
+	public void setD(double d) {
+		this.d = d;
+	}
+
+	public double getT() {
+		return t;
+	}
+
+	public void setT(double t) {
+		this.t = t;
+	}
+
+	public double getS() {
+		return s;
+	}
+
+	public void setS(double s) {
+		this.s = s;
+	}
+
+	public int getPhase() {
+		return phase;
+	}
+
+	public void setPhase(int phase) {
+		this.phase = phase;
+	}
+
+	public HelperLight getBase() {
+		return base;
+	}
+
+	public void setBase(HelperLight base) {
+		this.base = base;
+	}
+	
+	public Point2D getPoint(){
+		return new Point2D.Double(x, y);
+	}
+	
+}
