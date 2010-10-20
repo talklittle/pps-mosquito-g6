53a550f332391de8d71f251b98436b72ab927ce2
pare_ku@hotmail.com <pare_ku@hotmail.com@b202fef8-03fb-f63c-b751-98654c24a0e8>
2010-10-20 07:04:18 +0000

    WIP putlights stuff

--- trunk/f10_4444_mosquito_v1/src/mosquito/g6/PutLights.java
+++ trunk/f10_4444_mosquito_v1/src/mosquito/g6/PutLights.java
@@ -13,64 +13,85 @@ import mosquito.sim.Light;
 public class PutLights {
 	
 	private static Random random = new Random();
-	public static LinkedList<Point2D> list;
 	
-	//test
+	private static Set<HelperLight> putLightRandom(Set<Line2D> walls, Set<HelperLight> lights, int numLightLeft){
+		for ( ; numPlaced < numLightLeft; numPlaced++) {
+			do {
+				next = new Point2D.Double(random.nextDouble() * 100.0, random.nextDouble() * 100.0);
+				list.add(next);
+			} while (CollideWithWall.isCollideWithWall(next, walls));
+			l = new HelperLight(next.getX(), next.getY(), 1, 0, 0);
+			lights.add(l);
+		}
+		return lights;
+	}
 	
-	public static Set<Light> putLights(Set<Line2D> walls, Point2D base, int numLight, double initialRadient){
-		if(CollideWithWall.isCollideWithWall(base, walls) || OutOfBounds.isOutOfBounds(base)){
+	private static Set<HelperLight> putLightsRecursive(Set<Line2D> walls, HelperLight base, int numLightLeft, double radient, Set<HelperLight> lights, 
+			boolean reach360, double radius, int phase, int numLightPlaced){
+		
+		if(numLightLeft==0){
+			return lights;
+		}
+		if(numLightPlaced==20){
+			lights = putLightRandom(walls, lights, numLightLeft);
+			return lights;
+		}
+		// try to span flowery...  do each phase.
+		//hit wall, step small
+		//if collide with other lights, step small
+		if()
+		
+		
+		
+		
+		Point2D next;
+		int phase = 1;
+		int pedalIndex = 0;
+		int numPlacedThisPhase = 0;
+		int numLightPerPhase;
+		while(numPlaced < numLight){
+			if(pedalIndex==(phase*8)){
+				// If we couldn't place any during this phase, quit
+				if (numPlacedThisPhase == 0) {
+					// TODO a good way to place the remaining lights
+					
+				}
+				pedalIndex = 0;
+				numPlacedThisPhase = 0;
+				phase++;
+			}
+			numLightPerPhase = Math.min(phase*8, numLight);
+			next = new Point2D.Double(base.getX()+((19.9*phase)*Math.cos(initialRadient+(pedalIndex*2*Math.PI/numLightPerPhase))),
+					base.getY()+((19.9*phase)*Math.sin(initialRadient+(pedalIndex*2*Math.PI/numLightPerPhase))));
+			if(CollideWithWall.isCollideWithWall(base, next, walls) || OutOfBounds.isOutOfBounds(next)){
+				pedalIndex++;
+			}else{
+				list.add(next);
+				l = new Light(next.getX(), next.getY(), 60, 24, 24*(2-(phase-1)%3));
+				result.add(l);
+				pedalIndex++;
+				numPlaced++;
+				numPlacedThisPhase++;
+			}
+		}
+		return result;
+	}
+	
+	public static Set<HelperLight> putLights(Set<Line2D> walls, HelperLight base, int numLight){
+		
+		if(CollideWithWall.isCollideWithWall(base, walls) || OutOfBounds.isOutOfBounds(base.getPoint())){
 			return null;
 		}else{
-			int numPlaced = 0;
-			Light l = new Light(base.getX(),base.getY(), 1,1,1);
-			list = new LinkedList<Point2D>();
-			list.add(base);
-			Set<Light> result = new HashSet<Light>();
-			result.add(l);
+			HelperLight l = new HelperLight(base.getX(),base.getY(), 1,1,1);
+			l.setBase(null);
+			l.setPhase(0);
+			Set<HelperLight> lights = new HashSet<HelperLight>();
+			lights.add(base);
 			if(numLight==1){
-				return result;
+				return lights;
+			}else{
+				return putLightsRecursive(walls, base, numLight-1,0,lights,false,19.9, 1, 1);
 			}
-			numPlaced++;
-			
-			Point2D next;
-			int phase = 1;
-			int pedalIndex = 0;
-			int numPlacedThisPhase = 0;
-			int numLightPerPhase;
-			while(numPlaced < numLight){
-				if(pedalIndex==(phase*8)){
-					// If we couldn't place any during this phase, quit
-					if (numPlacedThisPhase == 0) {
-						// TODO a good way to place the remaining lights
-						for ( ; numPlaced < numLight; numPlaced++) {
-							do {
-								next = new Point2D.Double(random.nextDouble() * 100.0, random.nextDouble() * 100.0);
-								list.add(next);
-							} while (CollideWithWall.isCollideWithWall(next, walls));
-							l = new Light(next.getX(), next.getY(), 1, 0, 0);
-							result.add(l);
-						}
-						return result;
-					}
-					pedalIndex = 0;
-					numPlacedThisPhase = 0;
-					phase++;
-				}
-				numLightPerPhase = Math.min(phase*8, numLight);
-				next = new Point2D.Double(base.getX()+((19.9*phase)*Math.cos(initialRadient+(pedalIndex*2*Math.PI/numLightPerPhase))),
-						base.getY()+((19.9*phase)*Math.sin(initialRadient+(pedalIndex*2*Math.PI/numLightPerPhase))));
-				if(CollideWithWall.isCollideWithWall(base, next, walls) || OutOfBounds.isOutOfBounds(next)){
-					pedalIndex++;
-				}else{
-					list.add(next);
-					l = new Light(next.getX(), next.getY(), 60, 24, 24*(2-(phase-1)%3));
-					result.add(l);
-					pedalIndex++;
-					numPlaced++;
-					numPlacedThisPhase++;
-				}
-			}
-			return result;
 		}
 	}
 	public static Point2D.Double collectorPlace()
@@ -111,4 +132,5 @@ public class PutLights {
 		return collectorPlacement;
 		
 	}
+	
 }
